fbb7fdb6e9a0debe75dd30b1eefd6835515375d3
Patrick Sy <patrick.sy@get-it.us>
2021-05-01 00:07:35 +0200

    use global-jsdom to remove polyfill warnings

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -1,10 +1,21 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
 
+import * as Buffer from "buffer";
+import Web3 from 'web3';
+
 const reactLogo = require("./../assets/img/react_logo.svg");
 //import "./../assets/scss/App.scss";
 
-class App extends React.Component<Record<string, unknown>, any> {
+export default class App extends React.Component<Record<string, unknown>, any> {
+  componentDidMount() {
+//    console.log(web3.currentProvider);
+//    const metamask = new Web3(web3.currentProvider);
+    //const metamask = new Web3("ws://localhost:9545")
+    //metamask.eth.getAccounts().then(console.log);
+
+  }
+
   public render() {
     return (
       <div className="app">
@@ -15,4 +26,4 @@ class App extends React.Component<Record<string, unknown>, any> {
   }
 }
 
-export default hot(App);
+//export default hot(App);
