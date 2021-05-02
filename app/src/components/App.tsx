0794e9d929a5804ff9167e6531965e2a7f3bd3df
Patrick Sy <patrick.sy@get-it.us>
2021-05-02 22:25:14 +0200

    css reset

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
-import { hot } from "react-hot-loader";
+import {hot} from "react-hot-loader";
+import {Reset} from "styled-reset";
 
 import * as Buffer from "buffer";
 import Web3 from 'web3';
@@ -9,7 +10,7 @@ const reactLogo = require("./../assets/img/react_logo.svg");
 
 const red = "red";
 
-export const AppContainer = styled.div`
+export const AppDiv = styled.div`
   font-family: helvetica, arial, sans-serif;
   padding: 2em;
   border: 5px solid ${red};
@@ -17,12 +18,27 @@ export const AppContainer = styled.div`
 
 export const Paragraph = styled.p`
     background-color: yellow;
+    font-size: 24px;
+    line-height: 1.5;
+    padding: 1rem 0;
 `;
+
+export class AppContainer extends React.Component<Record<string, unknown>, any> {
+
+  public render() {
+    return (
+      <AppDiv>
+        <h1>Hello World!</h1>
+        <Paragraph>agagagag</Paragraph>
+      </AppDiv>
+    );
+  }
+}
 
 class App extends React.Component<Record<string, unknown>, any> {
   componentDidMount() {
-//    console.log(web3.currentProvider);
-//    const metamask = new Web3(web3.currentProvider);
+    //    console.log(web3.currentProvider);
+    //    const metamask = new Web3(web3.currentProvider);
     //const metamask = new Web3("ws://localhost:9545")
     //metamask.eth.getAccounts().then(console.log);
 
@@ -30,10 +46,10 @@ class App extends React.Component<Record<string, unknown>, any> {
 
   public render() {
     return (
-      <AppContainer>
-        <h1>Hello World!</h1>
-        <Paragraph>agagagag</Paragraph>
-      </AppContainer>
+      <React.Fragment>
+        <Reset />
+        <AppContainer />
+      </React.Fragment>
     );
   }
 }
--- app/tests/App.test.tsx
+++ app/tests/App.test.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import {shallow} from 'enzyme';
 import * as expect from 'chai';
-import App from '../src/components/App';
+import { AppContainer } from '../src/components/App';
 
 describe('App is rendered', () => {
     it("should do something", () => {
         // Render App in the document
-        const appNode = shallow(<App />);
+        const appNode = shallow(<AppContainer />);
 
         console.log(appNode.dive().length);
         // Verify text content
