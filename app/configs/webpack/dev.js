35e57a26570bd31b9b21b8ddf3bb998904430684
Patrick Sy <patrick.sy@get-it.us>
2022-04-12 23:48:33 +0200

    adding hotreload config

--- app/configs/webpack/dev.js
+++ app/configs/webpack/dev.js
@@ -8,12 +8,12 @@ module.exports = merge(commonConfig, {
   entry: [
     // "react-hot-loader/patch", // activate HMR for React
     "webpack-dev-server/client?http://localhost:8080", // bundle the client for webpack-dev-server and connect to the provided endpoint
-    // "webpack/hot/only-dev-server", // bundle the client for hot reloading, only- means to only hot reload for successful updates
+    "webpack/hot/only-dev-server", // bundle the client for hot reloading, only- means to only hot reload for successful updates
     "./index.tsx", // the entry point of our app
   ],
   devServer: {
     // transportMode: 'ws',
-    // hot: true, // enable HMR on the server
+    hot: true, // enable HMR on the server
   },
   devtool: "cheap-module-source-map",
   plugins: [
