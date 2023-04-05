a950cef88decc1c74c3074b6967de8a7e476b049
Patrick Sy <patrick.sy@get-it.us>
2023-04-05 17:53:42 +0200

    add prettier tailwindcss plugin for class ordering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
-<h1 class="text-3xl font-bold underline">
-  Welcome to SvelteKit with TailwindCss
-</h1>
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit with TailwindCss</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style lang="postcss">
