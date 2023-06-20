1c61d9e65ecb1e9d137011a763d40b43478b8f16
suiiii <suiiii@suiiii.rip>
2023-06-20 23:32:21 +0200

    using felte for form handling and validation

---
+++ src/lib/components/creator/NewCreator.svelte
@@ -0,0 +1,136 @@
+<script lang="ts">
+  import { matchEvents } from '$lib/web3/ethers';
+  import { toast } from '@zerodevx/svelte-toast';
+  import type { Creator } from '@createz/contracts/types/ethers-contracts/Creator';
+  import { CreatorMetadataSchema, type CreatorMetadata } from '$lib/web3/contracts/creator';
+  import { createForm } from 'felte';
+  import { validator } from '@felte/validator-zod';
+  import { reporter, ValidationMessage } from '@felte/reporter-svelte';
+
+  export let creator: Creator;
+  export let currentAccount: string;
+  export let onSuccess: (tokenId: bigint) => void;
+
+  let formDisabled = false;
+
+  const { form } = createForm<CreatorMetadata>({
+    async onSubmit(values) {
+      formDisabled = true;
+      try {
+        const tx = await creator.mint(
+          values.name,
+          values.description ?? '',
+          values.image ?? '',
+          values.external_url ?? ''
+        );
+        toast.push(`Transaction submitted: ${tx.hash}`, { pausable: true });
+
+        const receipt = await tx.wait();
+
+        const logs = receipt?.logs;
+        if (currentAccount) {
+          const res = await matchEvents(
+            logs as [],
+            creator,
+            creator.filters.Minted(currentAccount)
+          );
+          if (res[0]) {
+            const newTokenId = res[0].args.tokenId;
+            toast.push(`New token ID: ${newTokenId}`, { pausable: true });
+            onSuccess(newTokenId);
+          }
+        }
+      } catch (err) {
+        toast.push('Transaction failed', {
+          pausable: true,
+          theme: {
+            '--toastBackground': 'red',
+            '--toastColor': 'white',
+            '--toastBarBackground': 'fuchsia'
+          }
+        });
+        console.error('Transaction failed', err);
+      } finally {
+        formDisabled = false;
+      }
+    },
+    extend: [validator({ schema: CreatorMetadataSchema }), reporter]
+  });
+
+</script>
+
+<div>
+  <p>
+    Be aware: The more text you add to your profile the more you have to pay in gas to apply writing
+    changes.
+  </p>
+  <form use:form>
+    <fieldset>
+      <div>
+        <label for="name">Name:</label>
+        <input
+          id="name"
+          name="name"
+          placeholder="Your name"
+          disabled={formDisabled}
+          required
+          minlength="2"
+          class="bg-gray-500"
+        />
+        <ValidationMessage for="name" let:messages={messages}>
+          {#if messages}
+            <span>{messages}</span>
+          {/if}
+        </ValidationMessage>
+      </div>
+      <div>
+        <label for="description">Description:</label>
+        <textarea
+          id="description"
+          name="description"
+          disabled={formDisabled}
+          placeholder="Some text for people to recognize you"
+          class="bg-gray-500"
+        />
+        <ValidationMessage for="description" let:messages={messages}>
+          {#if messages}
+            <span>{messages}</span>
+          {/if}
+        </ValidationMessage>
+      </div>
+      <div>
+        <label for="image">Image:</label>
+        <input
+          id="image"
+          name="image"
+          disabled={formDisabled}
+          placeholder="https://example.com/my-image.png"
+          class="bg-gray-500"
+        />
+        <ValidationMessage for="image" let:messages={messages}>
+          {#if messages}
+            <span>{messages}</span>
+          {/if}
+        </ValidationMessage>
+      </div>
+      <div>
+        <label for="external_url">External URL:</label>
+        <input
+          id="external_url"
+          name="external_url"
+          disabled={formDisabled}
+          placeholder="https://www.my-website.com"
+          class="bg-gray-500"
+        />
+        <ValidationMessage for="external_url" let:messages={messages}>
+          {#if messages}
+            <span>{messages}</span>
+          {/if}
+        </ValidationMessage>
+      </div>
+      <div>
+        <button type="submit" disabled={formDisabled}>create</button>
+      </div>
+    </fieldset>
+  </form>
+</div>
--- src/routes/[network]/creator/new/+page.svelte
+++ src/routes/[network]/creator/new/+page.svelte
@@ -1,121 +1,20 @@
 <script lang="ts">
-  import { matchEvents } from '$lib/web3/ethers';
-  import { toast } from '@zerodevx/svelte-toast';
   import { currentAccount } from '$lib/web3/onboard';
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
   import { CREATOR_CONTRACT, requireContext } from '$lib/contexts';
   import type { Creator } from '@createz/contracts/types/ethers-contracts/Creator';
   import type { Readable } from 'svelte/store';
-  import { CreatorMetadataSchema, type CreatorMetadata } from '$lib/web3/contracts/creator';
+  import NewCreator from '$lib/components/creator/NewCreator.svelte';
 
   const creator = requireContext<Readable<Creator>>(CREATOR_CONTRACT);
 
-  let tokenData: CreatorMetadata = {
-    name: '',
-    description: '',
-    image: '',
-    external_url: ''
-  };
-
-  let formDisabled = false;
-
-  const mint = async () => {
-    formDisabled = true;
-    try {
-      const data = CreatorMetadataSchema.parse(tokenData);
-
-      const tx = await $creator.mint(
-        data.name,
-        data.description ?? '',
-        data.image ?? '',
-        data.external_url ?? ''
-      );
-      toast.push(`Transaction submitted: ${tx.hash}`, { pausable: true });
-
-      const receipt = await tx.wait();
-
-      const logs = receipt?.logs;
-      const acc = $currentAccount;
-      if (acc) {
-        const res = await matchEvents(logs as [], $creator, $creator.filters.Minted(acc));
-        if (res[0]) {
-          const newTokenId = res[0].args.tokenId;
-          toast.push(`New token ID: ${newTokenId}`, { pausable: true });
-          goto($page.url.pathname + `../${newTokenId}`); // TODO fix this
-        }
-      }
-    } catch (err) {
-      toast.push('Transaction failed', {
-        pausable: true,
-        theme: {
-          '--toastBackground': 'red',
-          '--toastColor': 'white',
-          '--toastBarBackground': 'fuchsia'
-        }
-      });
-      console.error('Transaction failed', err);
-    } finally {
-      formDisabled = false;
-    }
+  const onSuccess = (id: bigint) => {
+    goto($page.url.pathname + `../${id}`); // TODO fix this
   };
 </script>
 
 <h1>Create new Creator Token</h1>
 <p>contract: <span>{#await $creator.getAddress() then addr}{addr}{/await}</span></p>
 
-<div>
-  <p>
-    Be aware: The more text you add to your profile the more you have to pay in gas to apply writing
-    changes.
-  </p>
-  <form on:submit={mint}>
-    <fieldset>
-      <div>
-        <label for="name">Name:</label>
-        <input
-          id="name"
-          name="name"
-          bind:value={tokenData.name}
-          placeholder="Your name"
-          disabled={formDisabled}
-          required
-          minlength="2"
-        />
-      </div>
-      <div>
-        <label for="description">Description:</label>
-        <textarea
-          id="description"
-          name="description"
-          bind:value={tokenData.description}
-          disabled={formDisabled}
-          placeholder="Some text for people to recognize you"
-        />
-      </div>
-      <div>
-        <label for="image">Image:</label>
-        <input
-          id="image"
-          name="image"
-          bind:value={tokenData.image}
-          disabled={formDisabled}
-          placeholder="https://example.com/my-image.png"
-        />
-      </div>
-      <div>
-        <label for="externalUrl">External URL:</label>
-        <input
-          id="externalUrl"
-          name="externalUrl"
-          bind:value={tokenData.external_url}
-          disabled={formDisabled}
-          placeholder="https://www.my-website.com"
-        />
-      </div>
-      <div>
-        <button type="submit">create</button>
-      </div>
-    </fieldset>
-  </form>
-</div>
+<NewCreator creator={$creator} currentAccount={$currentAccount ?? ''} {onSuccess} />
