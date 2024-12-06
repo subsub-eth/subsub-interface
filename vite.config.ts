ae02380a9987eeb40c9444dd422097a7786a4f37
suiiii <suiiii@suiiii.rip>
2024-12-07 00:24:33 +0100

    refactor(svelte5): remove unneeded polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
-import { nodePolyfills as vitePluginNodePolyfills } from 'vite-plugin-node-polyfills';
-
 export default defineConfig({
   plugins: [
     sveltekit(),
-    vitePluginNodePolyfills({
-      // exclude: ['dns' /** vinejs wants this even though we are not using vine */],
-      protocolImports: true
-    })
   ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
